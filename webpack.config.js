7ea2779e728a64e9c98c7f70c316982ee81389a6
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-07-26 16:33:39 +0400

    test to fix 404 error vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const path = require('path');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist '),
-  },
-  // devServer: {
-  //   port: 4200,
-  // },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[contenthash].bundle.css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-    ],
-  },
-};
+// module.exports = {
+//   mode: process.env.NODE_ENV || 'development',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist '),
+//   },
+//   // devServer: {
+//   //   port: 4200,
+//   // },
+//   plugins: [
+//     new HTMLWebpackPlugin({
+//       template: './index.html',
+//     }),
+//     new CleanWebpackPlugin(),
+//     new MiniCssExtractPlugin({
+//       filename: '[contenthash].bundle.css',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//       },
+//       {
+//         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+//         use: 'url-loader?limit=10000',
+//       },
+//       {
+//         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+//         use: 'file-loader',
+//       },
+//     ],
+//   },
+// };
 
 // const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -63,3 +63,51 @@ module.exports = {
 // };
 
 // module.exports = config;
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  // mode: 'development',
+  // mode: 'production',
+  // mode: 'none',
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 4200,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
+
+module.exports = config;
